e64c66e4a86642170eab72b689838ed9ba0a359f
Ashwini54 <45494809+Ashwini54@users.noreply.github.com>
2021-08-25 17:02:53 +0530

    Update Dependencies.sh

--- Dependencies.sh
+++ Dependencies.sh
@@ -6,6 +6,7 @@ echo "##INSTALLING CATCH2  ##"
 apt-get -y update && \
     apt-get install -y build-essential && \
     apt-get install -y libgtest-dev &&  \
+    apt-get install cmake libgtest-dev
     apt-get install --assume-yes git && \ 
     apt-get install -y cmake && \
     apt-get install -y make 
