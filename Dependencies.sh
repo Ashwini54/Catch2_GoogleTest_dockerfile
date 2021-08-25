e1a97c4b349c444eae9979925a1350e9a4086459
Ashwini54 <45494809+Ashwini54@users.noreply.github.com>
2021-08-25 19:34:28 +0530

    Update Dependencies.sh

--- Dependencies.sh
+++ Dependencies.sh
@@ -9,14 +9,14 @@ apt-get -y update && \
     apt-get install -y libgtest-dev &&  \
     apt-get install -y cmake && \
     apt-get install -y make && \
-    cd /usr/src/gtest && \
-    cmake CMakeLists.txt && \
-    make && \
+#     cd /usr/src/gtest && \
+#     cmake CMakeLists.txt && \
+#     make && \
 #     apt-get install cmake libgtest-dev && \
     # copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
-    cp *.a /usr/lib && \
+#     cp *.a /usr/lib && \
     
-    apt-get install --assume-yes git && \ 
-    apt-get install -y make 
+#     apt-get install --assume-yes git && \ 
+#     apt-get install -y make 
     
 echo "## DONE ##" 
