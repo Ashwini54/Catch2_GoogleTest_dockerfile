7aba356349fb4d12bc960399c7efc4fdbb0c2913
Ashwini54 <45494809+Ashwini54@users.noreply.github.com>
2021-08-25 16:37:24 +0530

    Create Dependencies.sh

---
+++ Dependencies.sh
@@ -0,0 +1,14 @@
+echo "## UPDATING & INSTALLING PACKAGE ##"
+apt-get -y update 
+apt-get install -y tzdata 
+
+echo "##INSTALLING CATCH2  ##"
+apt-get -y update && \
+    apt-get install -y build-essential && \
+    apt-get install -y libgtest-dev &&  \
+    apt-get install --assume-yes git && \ 
+    apt-get install -y cmake && \
+    apt-get install -y make 
+    
+	
+echo "## DONE ##" 
