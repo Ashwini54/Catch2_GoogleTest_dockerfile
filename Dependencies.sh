06d7c60deaea19cce2b5506f83cd66367ea6b8f4
Ashwini54 <45494809+Ashwini54@users.noreply.github.com>
2021-08-25 17:17:15 +0530

    Update Dependencies.sh

--- Dependencies.sh
+++ Dependencies.sh
@@ -6,12 +6,15 @@ echo "##INSTALLING CATCH2  ##"
 apt-get -y update && \
     apt-get install -y build-essential && \
     apt-get install -y libgtest-dev &&  \
-    apt-get install cmake libgtest-dev && \
+    apt-get install -y cmake && \
+    cd /usr/src/gtest && \
+    cmake CMakeLists.txt && \
+    make && \
+#     apt-get install cmake libgtest-dev && \
     # copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
     cp *.a /usr/lib && \
+    
     apt-get install --assume-yes git && \ 
-    apt-get install -y cmake && \
     apt-get install -y make 
     
-	
 echo "## DONE ##" 
