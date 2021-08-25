d61648ec5dd439c7f46cf4cc7fbb63b820c27195
Ashwini54 <45494809+Ashwini54@users.noreply.github.com>
2021-08-25 15:34:23 +0530

    Update and rename Dependencies.sh to testing/catch2/Dependencies.sh

--- testing/catch2/Dependencies.sh
+++ testing/catch2/Dependencies.sh
@@ -2,14 +2,12 @@ echo "## UPDATING & INSTALLING PACKAGE ##"
 apt-get -y update 
 apt-get install -y tzdata 
 
-echo "##INSTALLING CATCH2 & GOOGLETEST ##"
+echo "##INSTALLING CATCH2  ##"
 apt-get -y update && \
     apt-get install -y build-essential && \
     apt-get install -y cmake protobuf-compiler &&  \
     apt-get install --assume-yes git && \ 
-    apt-get install -y make
-
-apt-get install -y libgtest-dev && \
-apt-get install -y cmake
+    apt-get install -y make && \
+    apt-get install -y cmake
 	
 echo "## DONE ##" 
