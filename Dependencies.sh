1f9342f4c2393e5bc2734647d8a2cd233b784add
Ashwini54 <45494809+Ashwini54@users.noreply.github.com>
2021-08-27 16:28:42 +0530

    Update Dependencies.sh

--- Dependencies.sh
+++ Dependencies.sh
@@ -2,18 +2,21 @@ echo "## UPDATING & INSTALLING PACKAGE ##"
 apt-get -y update 
 apt-get install -y tzdata 
 
-echo "##INSTALLING CATCH2  ##"
+echo "##INSTALLING GTEST  ##"
 apt-get -y update && \
     apt-get install -y build-essential && \
     apt-get install -y git && \
     apt-get install -y libgtest-dev &&  \
+    apt-get install --assume-yes git 
     apt-get install -y cmake && \
-    apt-get install -y make && \
+    apt-get install -y make 
+    
+    
 #     cd /usr/src/gtest && \
 #     cmake CMakeLists.txt && \
 #     make && \
 #     apt-get install cmake libgtest-dev && \
-    # copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
+# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
 #     cp *.a /usr/lib && \
     
 #     apt-get install --assume-yes git && \ 
